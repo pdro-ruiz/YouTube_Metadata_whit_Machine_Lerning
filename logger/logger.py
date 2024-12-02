23b6c4685d68d93315554c02e498fbdc0192b75e
Pedro Ruiz <32985142+pdro-ruiz@users.noreply.github.com>
2024-12-02 11:31:55 +0100

    Add files via upload

---
+++ logger/logger.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """
+    Configures a logger with the specified name and log file, along with a console handler.
+    
+    Parameters:
+        name (str): Name of the logger.
+        log_file (str): Path to the log file.
+        level (int): Logging level.
+    
+    Returns:
+        logging.Logger: Configured logger instance.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    
+    # Prevent multiple handlers from being added to the same logger
+    if not logger.handlers:
+        # Handler for the log file
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(level)
+        file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(file_formatter)
+        logger.addHandler(fh)
+        
+        # Handler for the console
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        ch.setFormatter(console_formatter)
+        logger.addHandler(ch)
+    
+    return logger
